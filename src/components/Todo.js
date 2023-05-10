1b97792d5400d8caa9dae46f3d025f724ad14c17
Nikhil Kumar Singh <kumarnishu94@gmail.com>
2023-05-10 15:37:34 +0530

    add edit functionality

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,53 +1,79 @@
 import React, { useEffect, useState } from "react";
 
-
-
 // to get data from localstorage
 
-const getLocalItem = ()=>{
-  let list = localStorage.getItem('lists')
+const getLocalItem = () => {
+  let list = localStorage.getItem("lists");
 
-  if(list){
-    return JSON.parse(localStorage.getItem('lists'));
-  }else{
+  if (list) {
+    return JSON.parse(localStorage.getItem("lists"));
+  } else {
     return [];
   }
-}
+};
 
 const Todo = () => {
   const [data, setData] = useState("");
   const [item, setItem] = useState(getLocalItem());
 
+  const [toggle, setToggle] = useState(true); // toggling add and edit icons state
+
+  const [edit, setEdit] = useState(null);
+
   const addItem = () => {
     if (!data) {
+      alert('please fill data')
+    } else if (data && !toggle) {
+      setItem(
+        item.map((elm) => {
+          if (elm.id === edit) {
+            return { ...elm, name: data }
+          }
+          return elm;
+        })
+      )
+      setToggle(true);
+      setData('');
+      setEdit(null)
     } else {
-      setItem([...item, data]);
+      const allData = { id: new Date().getTime().toString(), name: data }; // generate unique id here
+
+      setItem([...item, allData]);
       setData("");
     }
   };
 
   //delete items
-    const deletItem = (id) => {
-    const updateItem = item.filter((elm, ind) => {
-      return ind !== id;
+  const deletItem = (index) => {
+    const updateItem = item.filter((elm) => {
+      return index !== elm.id;
     });
     setItem(updateItem);
   };
 
-// remove all
+  // remove all
   const removeAll = () => {
     setItem([]);
   };
 
-
   //add data to local storage
 
-  useEffect(()=>{
-    localStorage.setItem('lists',JSON.stringify(item))
-  },[item])
+  useEffect(() => {
+    localStorage.setItem("lists", JSON.stringify(item));
+  }, [item]);
 
+  // for edit item
 
+  const editItem = (id) => {
+    const editItem = item.find((elm) => {
+      return elm.id === id;
+    });
 
+    setToggle(false)
+    setData(editItem.name);
+    setEdit(id)
+
+  };
 
   return (
     <>
@@ -58,28 +84,41 @@ const Todo = () => {
             <figcaption>Add Your Work</figcaption>
           </figure>
           <div className="additem">
+            {toggle ? (
+              <i
+                className="fa-solid fa-plus add-btn"
+                title="Add Item"
+                onClick={addItem}
+              ></i>
+            ) : (
+              <i
+                className="fa-solid fa-pen-to-square"
+                title="edit item"
+                onClick={addItem}
+              ></i>
+            )}
             <input
               type="text"
               placeholder="add items..."
               value={data}
               onChange={(e) => setData(e.target.value)}
             />
-            <i
-              className="fa-solid fa-plus add-btn"
-              title="Add Item"
-              onClick={addItem}
-            ></i>
           </div>
 
           <div className="showitem">
-            {item.map((elm, indx) => {
+            {item.map((elm) => {
               return (
-                <div className="eachitem" key={indx}>
-                  <h3>{elm}</h3>
+                <div className="eachitem" key={elm.id}>
+                  <h3>{elm.name}</h3>
+                  <i
+                    className="fa-solid fa-pen-to-square"
+                    title="edit item"
+                    onClick={() => editItem(elm.id)}
+                  ></i>
                   <i
                     className="fa-sharp fa-solid fa-trash"
                     title="delete item"
-                    onClick={() => deletItem(indx)}
+                    onClick={() => deletItem(elm.id)}
                   ></i>
                 </div>
               );
